b8dfd346048cb7535e851b3ee9006bff050e85bf
Rene Benjamin Ulloa <renebulloa@renes-mbp.myfiosgateway.com>
2021-05-08 15:43:01 -0400

    assignment 3 results

--- a3/a3.R
+++ a3/a3.R
@@ -12,7 +12,7 @@ View(sumdata)
 
 # data goes from 2002-Jan to 2019-Dec
 #plot(sumdata$crime_month, sumdata$crimes)
-# create a time series object and time series plot
+# we create a time series object and time series plot
 
 crimedta <- ts(sumdata[,c("crimes")], start=c(2002,1), frequency=12)
 plot(crimedta, type="l")
@@ -64,7 +64,7 @@ View(dfmerge2pp)
 # rename the variable
 dfmerge2pp$ppcrime_per_resident <- dfmerge2pp$totalcrime_type/dfmerge2pp$tot_pop
 
-dfmerge2pp1 <- dfmerge2pp[, c(1,3,21)]
+dfmerge2pp1 <- dfmerge2pp[, c(1,3,18)]
 dfmerge2pp1$crime_month <- dfmerge2pp1$month
 dfmerge2pp2 <- dfmerge2pp1[, c(2:4)]
 
@@ -86,7 +86,7 @@ dfmerge2vi <- dfmerge2[dfmerge2$crime_type == "violent", ]
 # rename the variable
 dfmerge2vi$vicrime_per_resident <- dfmerge2vi$totalcrime_type/dfmerge2vi$tot_pop
 View(dfmerge2vi)
-dfmerge2vi1 <- dfmerge2vi[, c(1,3,21)]
+dfmerge2vi1 <- dfmerge2vi[, c(1,3,18)]
 dfmerge2vi1$crime_month <- dfmerge2vi1$month
 dfmerge2vi2 <- dfmerge2vi1[, c(2:4)]
 
@@ -106,7 +106,7 @@ View(df3)
 df3$crime_month <- df3$month
 df3$district <- df3$unit
 
-dfsubset <- dfmerge4[,c(1:9, 12:21)]
+dfsubset <- dfmerge4[,c(1:9, 12:18)]
 
 mergedta3 <- left_join(df3, dfsubset, by=c("district","crime_month"))
 View(mergedta3)
